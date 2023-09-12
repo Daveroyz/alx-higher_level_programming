4d029a63fd0ea87d76ce11a79143baca6788b9d5
Daveroyz <davidsewu65@gmail.com>
2023-09-12 15:27:34 -0700

    all task

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // create an empty object
+    }
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      console.log();
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
